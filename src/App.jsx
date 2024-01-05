228eafdede8042d287b3f65399c0534f81204ee1
sabzevari73 <webclubfc@gmail.com>
2024-01-05 15:01:01 +0330

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BiologyQuestions from './assets/components/BiologyQuestions/BiologyQuestions'
+
+const App = () => {
+  return (
+    <>
+      <BiologyQuestions/>
+    </>
+  )
+}
+
+export default App
